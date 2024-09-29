513aa9ae7ec5f2312d7b81ab811fbc1bdae47a55
“Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-09-29 22:15:08 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import {Routes} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import Home from './components/Home';
 import Products from './components/Products';
+import Header from './components/Layout/Header';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
-    <div >
+    <div>
+        <Header />
+        <main>
       <Route path='/home'>
         <Home />
       </Route>
-      <Route path='products'>
+      <Route path='/products'>
         <Products />
       </Route>
+      <Route path='/product-details'>
+      <ProductDetails />
+      </Route>
+        </main>
     </div>
   );
 }
