bd7a7028f62f83b1cd7e3e9aad0dc857852cff60
“Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-10-03 13:25:13 +0200

    Removed authContext

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/authContext";
 import { ShoppingState } from "./context/shopping/ShoppingState";
 
 ReactDOM.render(
   <BrowserRouter>
-    <AuthContextProvider>
       <ShoppingState>
         <App />
       </ShoppingState>
-    </AuthContextProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
